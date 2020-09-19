7442a2664e851b70872e1c77285ec721415edeb4
Vladislav Sosiuk <v.sosiuk@alterplay.com>
2020-09-19 15:54:27 +0300

    Initial Commit

---
+++ ReduxPrototype/ReduxPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxPrototypeApp.swift
+//  ReduxPrototype
+//
+//  Created by Vladislav Sosiuk on 19.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct ReduxPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
